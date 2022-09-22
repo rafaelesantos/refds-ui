591f0f0084885bb5a836dd9d7b3f8e508eb701e2
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-09-22 16:54:30 -0400

    Add medium size to corner radius

--- Sources/RefdsUI/Foundation/Borders/BorderRadius.swift
+++ Sources/RefdsUI/Foundation/Borders/BorderRadius.swift
@@ -3,5 +3,6 @@ import CoreGraphics
 public enum BorderRadius {
     public static let desktop: CGFloat = 3
     public static let `default`: CGFloat = 6
+    public static let medium: CGFloat = 12
     public static let large: CGFloat = 16
 }
