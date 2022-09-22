5eea4c2883178f4fbe96c2c4815fcb8ea1f4c900
Rafael Escaleira <rafael.escaleira@deliverit.com.br>
2022-09-22 00:37:01 -0400

    Add WIP

---
+++ Sources/RefdsUI/WIP/AssetNameProviding.swift
@@ -0,0 +1,14 @@
+public protocol AssetNameProviding {
+    var assetName: String { get }
+}
+
+extension AssetNameProviding {
+    public var assetName: String {
+        defaultAssetName
+    }
+
+    public var defaultAssetName: String {
+        let name = String(describing: self)
+        return "\(name.prefix(1).capitalized)\(name.dropFirst())"
+    }
+}
---
+++ Sources/RefdsUI/WIP/Bundle.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+private class CurrentBundleFinder {}
+
+public extension Bundle {
+    static var current: Bundle = {
+        let bundleName = "RefdsUI_RefdsUI"
+
+        let candidates = [
+            main.resourceURL,
+            Bundle(for: CurrentBundleFinder.self).resourceURL,
+            main.bundleURL,
+            Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
+        ]
+
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+
+            let bundleRootPath = candidate?.appendingPathComponent("../" + bundleName + ".bundle")
+            if let bundle = bundleRootPath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+
+        fatalError("unable to find bundle named \(bundleName)")
+    }()
+}
